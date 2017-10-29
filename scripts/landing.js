937bfc35d337372d1191b645782089972e0ee2ed
Hannah Debenham <hannahdebs213@gmail.com>
2017-10-29 13:07:50 -0400

    Checkpoint-22

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,7 @@
-var animatePoints = function( ) {
-                var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
                 
+
+var animatePoints = function(points) {
                 var revealPoint = function( ) {
                     for(var i = 0; i < 3; i++){
                         points[i].style.opacity = 1;
@@ -11,4 +12,21 @@ var animatePoints = function( ) {
                 };
 
                 revealPoint();
-            };
+};
+
+window.onload = function() {
+    
+    //Automatically animate the points on load when scrolling won't trigger animation
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    }); 
+}
